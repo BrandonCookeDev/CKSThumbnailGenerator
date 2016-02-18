3e01c2e9cc469212cea9023c83f3c32d75156743
Cooke, Brandon (bc719c) <bc719c@us.att.com>
2016-02-18 16:23:59 -0500

    Got the simple server actually running and talking to the angular script.

--- server/app.js
+++ server/app.js
@@ -1,5 +1,3 @@
-console.log("test");
-
 // set up ========================
     var express  = require('express');
     var app      = express();                               // create our app w/ express
@@ -13,16 +11,16 @@ console.log("test");
 
     //mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');     // connect to mongoDB database on modulus.io
 
-    app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+    app.use(express.static(__dirname + '/public'));
+    //app.use(express.static(__dirname + '/client'));
+    //app.use(express.static(__dirname + '/node_modules'));
+    
     app.use(morgan('dev'));                                         // log every request to the console
     app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
     app.use(bodyParser.json());                                     // parse application/json
     app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
     app.use(methodOverride());
 
-    console.log(__dirname);
-    app.use(express.static(__dirname + '/../node_modules'));
-    app.use(express.static(__dirname + '/../client/scripts'));
     app.get('/', function(req, res){
     	var p = path.join(__dirname + '/../client/index.html');
     	console.log(p);
