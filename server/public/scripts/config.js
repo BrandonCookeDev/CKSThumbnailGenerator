4d5c41bea24ad96f4f9f62f13fa2aed1e35bb9fd
Cooke, Brandon (bc719c) <bc719c@us.att.com>
2016-05-24 14:49:17 -0400

    Added logic to center the characters towards the middle more.

--- server/public/scripts/config.js
+++ server/public/scripts/config.js
@@ -4,18 +4,18 @@ var p2x = null; var p2y = 50;
 var p3x = null; var p3y = 50;
 var p4x = null; var p4y = 50;
 
-var xOffset = 40;
+var xOffset = 70;
 var yOffset = 15;
 
 var getCharacterCoordinates = function(canvas, image, characterNumber){
 	this.canvas = canvas;
 	switch(characterNumber){
 	case 1:
-		p1x = -1 * xOffset;
+		p1x = canvas.width/2 - image.width - xOffset;
 		p1y = canvas.height - image.height + yOffset;
 		return {x:p1x, y:p1y};
 	case 2:
-		p2x = canvas.width - image.width + xOffset;
+		p2x = canvas.width/2;
 		p2y = canvas.height - image.height + yOffset;
 		return {x:p2x, y:p2y};
 	case 3:
